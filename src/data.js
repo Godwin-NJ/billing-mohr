92b970918b6b5fa42822e414f774c87023b8bf91
Godwin-NJ <amadigodwin7@gmail.com>
2022-04-02 09:38:14 +0100

    project init

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const Navigation = [
+  {
+    id: 1,
+    nav: "general",
+    icon: "***",
+  },
+  {
+    id: 2,
+    nav: "password",
+    icon: "***",
+  },
+  {
+    id: 3,
+    nav: "invitations",
+    icon: "***",
+  },
+  {
+    id: 4,
+    nav: "billing",
+    icon: "***",
+  },
+  {
+    id: 5,
+    nav: "apps",
+    icon: "***",
+  },
+];
